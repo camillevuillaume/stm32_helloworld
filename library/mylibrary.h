ad88de92025f520d549bde6609c03ad4b11342d4
Camille Vuillaume <camillevuillaume@gmail.com>
2024-01-05 20:34:13 +0900

    added needed files to git

---
+++ library/mylibrary.h
@@ -0,0 +1,6 @@
+#ifndef __EMBEDDEDCRYPTOLIB_H
+#define __EMBEDDEDCRYPTOLIB_H
+
+void print_hello();
+
+#endif
---
+++ toolchain.cmake
@@ -0,0 +1,18 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER "/usr/bin/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/arm-none-eabi-g++")
+
+
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Og -Wall -fdata-sections -ffunction-sections")
+set(ASM_OPTIONS "-x assembler-with-cpp")
+set(CMAKE_ASM_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Og -Wall -fdata-sections -ffunction-sections")
